1d40d08ccda78d8c3e4fd27c7f8dbb92fdd6ede3
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-17 01:59:26 +0800

    fix: added method assignLeader and isLeader to Klass

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,10 +1,12 @@
 package ooss;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class Klass {
 
     private final int number;
+    private String leader = "";
 
     public Klass(int number) {
         this.number = number;
@@ -23,7 +25,26 @@ public class Klass {
         return Objects.hash(number);
     }
 
+
+
     public int getNumber() {
         return number;
+    }
+
+    public void assignLeader(Student student) {
+        if(Optional.ofNullable(student.getClassNumber()).orElse(0) == this.number){
+            leader = student.getName();
+        } else {
+            System.out.println("It is not one of us.");
+        }
+
+    }
+
+    public boolean isLeader(Student student) {
+        return student.getName().equals(leader);
+    }
+
+    public void attach(Teacher teacher) {
+
     }
 }
