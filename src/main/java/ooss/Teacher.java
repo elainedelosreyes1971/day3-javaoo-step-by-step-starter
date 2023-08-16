508261ec0f59dae6d3e7aaa733d7269441e298da
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-16 23:52:30 +0800

    fix: added belongsTo and assignTo methods

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -6,12 +6,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
+
+    private String teacherInfo = "";
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return super.introduce().concat(" I am a teacher.");
+        return super.introduce().concat(" I am a teacher.").concat(teacherInfo);
+    }
+
+    public void assignTo(Klass klass) {
+        if(klass != null){
+            teacherInfo = "I teach class " + klass.getNumber();
+        }
+    }
+
+    public boolean belongsTo(Klass klass) {
+
+        return teacherInfo.contains("class " + klass.getNumber());
     }
 }
