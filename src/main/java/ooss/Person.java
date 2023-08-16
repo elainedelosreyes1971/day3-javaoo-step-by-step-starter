9b7bfeac84e7483e46af3e9228433e1ebbbfcc09
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-16 18:18:30 +0800

    fix: added equals and hashCode method for step1 Test2 checking

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,5 +1,7 @@
 package ooss;
 
+import java.util.Objects;
+
 public class Person {
 
     private final int id;
@@ -14,5 +16,18 @@ public class Person {
 
     public String introduce() {
         return "My name is " + name + ". I am "+ age +" years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 }
